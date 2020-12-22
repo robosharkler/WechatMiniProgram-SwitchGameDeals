7b99a6a07f41d1e926d9d82a34c6531dfaf12357
shaohua-yuan <mr.shaohuay@gmail.com>
2020-12-22 16:35:56 -0600

    ugly page

--- frontend/pages/AmericaGameDeals/AmericaGameDeals.js
+++ frontend/pages/AmericaGameDeals/AmericaGameDeals.js
@@ -5,6 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tableData:{
+      title:["Artbox", "Title", "Price", "salePrice", "LowestPrice", "Detail"],
+      countentItem:[
+      ],
+    },
     
   },
 
@@ -12,7 +17,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'https://switch-game-deals.herokuapp.com/games/eshop-sales',
+      success:function(res){
+        var listDataBuffer = res.data.data
+        var listData = []
+        listDataBuffer.forEach(element => {
+          // var pushElem = { boxart : element.boxart, title:element.title, price: element.price,
+          //   salePrice:element.salePrice, lowestPrice: element.lowestPrice, detail: element.url}
+          var pushElem = [element.boxart,element.title,element.price,element.salePrice,element.salePrice,element.lowestPrice,element.url]
+          listData.push(pushElem)
+        });
 
+        that.setData({
+            tableData:{
+            title:["Boxart", "Title", "Price", "salePrice", "LowestPrice", "Detail"],
+            countentItem:listData,
+          },
+        })
+        console.log(that.data.tableData.countentItem.length)
+        console.log(that.data.tableData.title)
+        console.log(that.data.tableData.countentItem[0])
+      }
+    })
   },
 
   /**
