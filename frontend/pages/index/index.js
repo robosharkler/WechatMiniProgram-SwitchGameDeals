ebef7e03fea4c8fe20d28e2e385c69466d0da2ac
shaohua-yuan <mr.shaohuay@gmail.com>
2020-12-22 18:26:47 -0600

    improved naming

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -3,13 +3,9 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/250px-Flag_of_the_United_States.svg.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    americaLogoURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/250px-Flag_of_the_United_States.svg.png',
   },
-  getUSGameDeals: function(e) {
+  getAmericaGameDeals: function(e) {
     wx.navigateTo({
       url: '../AmericaGameDeals/AmericaGameDeals',
     })
